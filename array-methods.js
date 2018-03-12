c850ac4d2a4bc4301305281c0482b5bc8a545265
Phonethip <phonethip.liu@gmail.com>
2018-03-11 22:05:03 -0500

    filter method done

--- array-methods.js
+++ array-methods.js
@@ -19,29 +19,34 @@ planets.forEach( (printPlanets) => {
 
 
 /*
-    Use the map method to create a new array where the 
-    first letter of each planet is capitalized. Use the
-    `toUpperCase()` method on strings.
+    Use the map method to create a new array where the first letter of each planet is capitalized. Use the `toUpperCase()` method on strings.
 
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 */
 const print = document.getElementById("planets-caps");
 print.innerHTML = "<h3> Planets using map method to create a new array where the first letter of each planet is capitalized:</h3>";
 
-planets.map( (caps)=> {
+planets.map( (caps) => {
     let capPlanets = caps.charAt(0).toUpperCase() + caps.slice(1);
     print.innerHTML += `<p> ${capPlanets} </p>`;
-    console.log(capPlanets);
+    // console.log(capPlanets);
     return capPlanets;
 });
 
 /*
-    Use the filter method to create a new array that
-    contains planets with the letter 'e'. Use the `includes()`
-    method on strings.
+    Use the filter method to create a new array that contains planets with the letter 'e'. Use the `includes()` method on strings.
 
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
 */
+
+var eplanets= document.getElementById("planets-e");
+eplanets.innerHTML = "<h3> Planets filter method to create a new array that contains planets with the letter 'e':</h3>";
+
+var newPlanetsArray = planets.filter( (planet) => {
+    return planet.includes("e");   
+});
+// console.log("newPlanetsArray", newPlanetsArray);
+newPlanetsArray = eplanets.innerHTML += `<p> ${newPlanetsArray.join(" , ")} </p>`;
 
 
 // Use the reduce method to create a sentence from the words in the following array
